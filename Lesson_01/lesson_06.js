f5182d0a3b2430cd624156b0e2344af59ce94314
lichaoqun <121708983@qq.com>
2017-11-12 22:59:27 +0800

    x
    x

--- Lesson_01/lesson_06.js
+++ Lesson_01/lesson_06.js
@@ -1,3 +1,15 @@
 /**
  * Created by lichaoqun on 2017/11/8.
  */
+//====================Symbol=============
+// - 一般的 Symbol 的使用
+let getName = Symbol('name');
+let obj = {
+    [getName](){
+        return 'QG';
+    }
+}
+
+console.log(obj[getName]());
+
+// - 
