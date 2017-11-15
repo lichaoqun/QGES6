aa1a869636d671d6db366e7b298c04361ca1c807
QG <121708983@qq.com>
2017-11-15 13:48:34 +0800

    xx

--- Lesson_01/lesson_11.js
+++ Lesson_01/lesson_11.js
@@ -1,3 +1,26 @@
 /**
  * Created by lichaoqun on 2017/11/15.
  */
+//========================== import 和 export ============================
+import {name} from './lesson_11_module';
+console.log(name); // QG
+
+import {name as myName, User1 as myUser, default as changeName2_1} from './lesson_11_module';
+console.log(myName);// QG
+
+import  * as me from './lesson_11_module';
+console.log(me);
+
+import  {myGame} from './lesson_11_module';
+console.log(myGame);
+
+import  {myGame as myGame2} from './lesson_11_module';
+console.log(myGame2);
+
+import * as obj from './lesson_11_module';
+console.log(obj.age);
+console.log(obj.changeAge());
+console.log(obj.User3);
+
+// - 这里的 me 是 default export 导出的变量, 后边的是 export 导出的变量
+import me, {name, changeAge}from './lesson_11_module';
--- Lesson_01/lesson_11_module.js
+++ Lesson_01/lesson_11_module.js
@@ -1,4 +1,56 @@
 /**
  * Created by lichaoqun on 2017/11/15.
  */
+//========================== import 和 export ============================
+// - 一个一个导出
 export var name = 'QG';
+export class User{
+    constructor(name){
+        this.name = name;
+    }
+}
+export  function  changeName (_name){
+    name = _name;
+}
+
+// - 一起导出
+var name1 = 'QG';
+class User1{
+    constructor(name){
+        this.name = name;
+    }
+}
+function  changeName1 (_name){
+    name = _name;
+}
+export {name1, User1, changeName1};
+
+// - 修改名称导出
+export  default function changeName2(_name) {
+    name = _name;
+}
+
+var game = 'LOL';
+export {game as myGame};
+
+// - 附着在对象上导出
+export var age = 10;
+export function  changeAge(_age){
+    age = _age;
+}
+export  class User3{
+    constructor(name){
+        this.name = name;
+    }
+
+    changeAge(age){
+        this.age = age;
+    }
+}
+
+// - 导出默认的变量 并且通过 default 导出的是不可以改变值的 其他的导出是可以改变值的
+var age2 = 222;
+export default age2;
+setTimeout(function(){
+    age2 = 555
+});
